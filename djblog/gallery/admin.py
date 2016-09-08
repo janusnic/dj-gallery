73bf2d9f281583066c10fb5da88eae312f36fd22
Janus Nicon <janusnic@gmail.com>
2016-09-08 07:35:00 +0300

    added stage 3

--- djblog/gallery/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Album, Photo, Category
-# Register your models here.
-
-admin.site.register(Album)
-admin.site.register(Photo)
-admin.site.register(Category)
